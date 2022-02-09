322eb5ec28e5041504a825453a666b31d9ece934
Lukas Koch <luk4skoch@gmail.com>
2022-02-09 12:45:54 +0100

    mergetest

---
+++ test.py
@@ -0,0 +1,19 @@
+#with open('/home/luk4skoch/projects/hangman/countries-and-capitals.txt') as file:
+  #  data = file.read()
+  #  print(data)
+import os
+
+#for f in os.listdir("/home/luk4skoch/projects/hangman"):
+	#print(f)
+
+#print(os.listdir())
+
+os.getcwd()
+
+print(os.getcwd())
+
+#os.chdir("hangman/")
+
+with open('countries-and-capitals.txt') as file:
+    data = file.read()
+    print(data)
